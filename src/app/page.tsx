5a6b075494a0e710b186e9a6b1100bba41364a37
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-04-11 10:03:25 +0300

    check if session exists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { authClient } from '@/lib/auth-client'
 
 export default function Home() {
+  const {data: session} = authClient.useSession()
+  console.log('user', session?.user)
   const handleSignIn = async () => {
     const data = await authClient.signIn.social({provider: 'github'})
     console.log(data)
@@ -10,6 +12,14 @@ export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <h1>Hello</h1>
+      {session?.user && (
+        <div>
+          <h2>{session.user.name}</h2>
+          <p>{session.user.email}</p>
+          <img src={session.user.image || ''} alt="profile" />
+        </div>
+      )}
       <button onClick={handleSignIn}>Sign in with Github</button>
     </div>
   );
