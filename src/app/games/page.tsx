9b50675cb2762f5901edb7d61fcd7ae839718661
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-05-19 13:30:32 +0300

    games api for school

---
+++ src/app/games/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+export default function page() {
+    const fetchGenre = async (genre:string) => {
+        const res = await fetch(`/api/games?genre=${genre}`)
+        const data = await res.json()
+        console.log(data)
+    }
+    fetchGenre("RPG")
+  return (
+    <div>page</div>
+  )
+}
